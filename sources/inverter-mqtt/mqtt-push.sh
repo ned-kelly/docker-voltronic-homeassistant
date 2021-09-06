a9f05c9dc5894573be8ceb76e9423ec527a3a39b
Ian Evans <ian@techne.net>
2021-09-05 17:42:22 -0700

    Adding missing $MQTT_SERIAL variable.

--- sources/inverter-mqtt/mqtt-push.sh
+++ sources/inverter-mqtt/mqtt-push.sh
@@ -6,6 +6,7 @@ pushMQTTData () {
     MQTT_PORT=`cat /etc/inverter/mqtt.json | jq '.port' -r`
     MQTT_TOPIC=`cat /etc/inverter/mqtt.json | jq '.topic' -r`
     MQTT_DEVICENAME=`cat /etc/inverter/mqtt.json | jq '.devicename' -r`
+    MQTT_SERIAL=`cat /etc/inverter/mqtt.json | jq '.serial' -r`
     MQTT_USERNAME=`cat /etc/inverter/mqtt.json | jq '.username' -r`
     MQTT_PASSWORD=`cat /etc/inverter/mqtt.json | jq '.password' -r`
 
@@ -124,4 +125,3 @@ Battery_redischarge_voltage=`echo $INVERTER_DATA | jq '.Battery_redischarge_volt
 
 Warnings=`echo $INVERTER_DATA | jq '.Warnings' -r`
 [ ! -z "$Warnings" ] && pushMQTTData "Warnings" "$Warnings"
-
