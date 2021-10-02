73aa98678cf7d33dc008a2d6e54d079ce67e389c
Keith Chiem <chiem@yahoo.com>
2021-10-02 02:08:39 -0700

    - loop the subscriber process to handle mqtt server restarts - reduce unnecessary wrapper processes

--- sources/inverter-mqtt/entrypoint.sh
+++ sources/inverter-mqtt/entrypoint.sh
@@ -6,10 +6,11 @@ export TERM=xterm
 # Init the mqtt server for the first time, then every 5 minutes
 # This will re-create the auto-created topics in the MQTT server if HA is restarted...
 
-watch -n 300 /opt/inverter-mqtt/mqtt-init.sh > /dev/null 2>&1 &
+watch -xn 300 /opt/inverter-mqtt/mqtt-init.sh &
 
 # Run the MQTT Subscriber process in the background (so that way we can change the configuration on the inverter from home assistant)
-/opt/inverter-mqtt/mqtt-subscriber.sh &
+# This normally doesn't exit, but the watch is needed to handle mqtt server restarts.
+watch -xn 1 /opt/inverter-mqtt/mqtt-subscriber.sh &
 
 # execute exactly every 30 seconds...
-watch -n 30 /opt/inverter-mqtt/mqtt-push.sh > /dev/null 2>&1
+watch -xn 30 /opt/inverter-mqtt/mqtt-push.sh
